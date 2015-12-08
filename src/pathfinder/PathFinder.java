971484f8eb3d1a1adeaa187722056c833cf55d5a
Pawel Krynski <krynskip@ee.pw.edu.pl>
2015-12-08 18:32:25 +0100

    Calculate all available paths from a given Vertex.

--- src/pathfinder/PathFinder.java
+++ src/pathfinder/PathFinder.java
@@ -1,5 +1,7 @@
 package pathfinder;
 
+import java.util.PriorityQueue;
+
 /**
  *
  * @author Paweł
@@ -10,7 +12,61 @@ public class PathFinder {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Vertex one = new Vertex("A");
+        Vertex two = new Vertex("B");
+        Vertex three = new Vertex("C");
+
+        Edge[] oneNeighbours = new Edge[2];
+        oneNeighbours[0] = new Edge(two, 2);
+        oneNeighbours[1] = new Edge(three, 5);
+        
+        one.neighbours = oneNeighbours;
+
+        Edge[] twoNeighbours = new Edge[1];
+        twoNeighbours[0] = new Edge(three, 4);
+        
+        two.neighbours = twoNeighbours;
+        
+        System.out.println("Available paths from A:");
+        calculatePathsFrom(one);
+        
+        System.out.println("\nAvailable paths from B:");
+        calculatePathsFrom(two);
+
+    }
+
+    public static void calculatePathsFrom(Vertex source) {
+
+        source.minDist = 0;
+        PriorityQueue<Vertex> visited = new PriorityQueue<>();
+
+        visited.add(source);
+
+        while (!visited.isEmpty()) {
+
+            Vertex currentStart = visited.poll();
+
+            for (Edge e : currentStart.neighbours) {
+
+                Vertex currentEnd = e.end;
+                double weight = e.weight;
+                
+                System.out.println("From: " + source.toString() + " to: " + currentEnd.toString() + " - " + weight);
+
+                double currentPathCost = currentStart.minDist + weight;
+
+                if (currentPathCost < currentStart.minDist) {
+
+                    visited.remove(currentEnd);
+                    currentEnd.minDist = currentPathCost;
+                    currentEnd.previous = currentStart;
+                    visited.add(currentEnd);
+                }
+            }
+
+        }
+
     }
 
 }
@@ -25,7 +81,7 @@ class Vertex implements Comparable<Vertex> {
     public Vertex(String newName) {
         this.name = newName;
     }
-    
+
     @Override
     public String toString() {
         return this.name;
@@ -43,8 +99,8 @@ class Edge {
     public Vertex end;
     public double weight;
 
-    public Edge(Vertex target, double w) {
-        this.end = target;
+    public Edge(Vertex destination, double w) {
+        this.end = destination;
         this.weight = w;
     }
 }
