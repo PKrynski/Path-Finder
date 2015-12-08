3c6cfb9a35de181f6da02b828c17d23db5285b31
Pawel Krynski <krynskip@ee.pw.edu.pl>
2015-12-08 17:31:24 +0100

    Initial commit.

---
+++ src/pathfinder/PathFinder.java
@@ -0,0 +1,16 @@
+package pathfinder;
+
+/**
+ *
+ * @author Paweł
+ */
+public class PathFinder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
