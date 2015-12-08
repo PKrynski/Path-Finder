4fa886be595498302756197ed94262b4ee8f23f6
Pawel Krynski <krynskip@ee.pw.edu.pl>
2015-12-08 17:48:51 +0100

    Added Vertex and Edge representations.

--- src/pathfinder/PathFinder.java
+++ src/pathfinder/PathFinder.java
@@ -12,5 +12,39 @@ public class PathFinder {
     public static void main(String[] args) {
         // TODO code application logic here
     }
+
+}
+
+class Vertex implements Comparable<Vertex> {
+
+    public String name;
+    public Edge[] neighbours;
+    public double minDist = Double.POSITIVE_INFINITY;
+    public Vertex previous;
+
+    public Vertex(String newName) {
+        this.name = newName;
+    }
     
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    @Override
+    public int compareTo(Vertex second) {
+        return Double.compare(this.minDist, second.minDist);
+    }
+
+}
+
+class Edge {
+
+    public Vertex end;
+    public double weight;
+
+    public Edge(Vertex target, double w) {
+        this.end = target;
+        this.weight = w;
+    }
 }
